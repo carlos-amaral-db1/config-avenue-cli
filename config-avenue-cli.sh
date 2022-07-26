9af30c28ed966e3a3a5c2294b73653c680a3a84d
Carlos Alberto Barcelos Amaral <carlos.amaral@>
2022-07-26 12:06:03 -0300

    Changes to be committed: 	new file:   config-avenue-cli.sh 	new file:   notes

---
+++ config-avenue-cli.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Let's config your Avenue CLI"
+echo
+echo
+echo "---------------------------------"
+echo "Downloading dependencies packages"
+# wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb
+# sudo dpkg -i wkhtmltox_0.12.5-1.focal_amd64.deb
+# sudo snap install task --classic
+
+# git checkout dev
+# git pull origin dev
+
+# echo "Path of avenuesec"
+# echo "Something like: /home/yourname/go/src/github.com/avenuesec"
+# echo "or you can use: pwd (lowercase)"
+# read path_avenuesec
+
+# if $path_avenuesec == "pwd"
+# then
+#     $path_avenuesec = pwd
+# fi
+
+# export AVENUE_HOME=$path_avenuesec
+# git pull origin main
